cbd1913f470b27ed13b31ed63851a37ef3947e86
leianhua <leianhua1@flkj.ai>
2021-11-24 09:17:05 +0800

    first commit

--- app01/views.py
+++ app01/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render,HttpResponse
-
+from django.shortcuts import render, HttpResponse
+from templates import *
 # Create your views here.
+
+
+def index(request):
+
+    return render(request, 'index.html')
 
 
 def test(request):
--- web_sys/urls.py
+++ web_sys/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from app01 import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index),
     path('test/', views.test),
 ]
